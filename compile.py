a27bf09dd4927e054148750658398da5896fe652
StealthHydra179 <aidenm888@gmail.com>
2021-06-03 18:52:15 -0400

    Update Gitignore, and make compiler a bit more detailed

--- compile.py
+++ compile.py
@@ -6,8 +6,10 @@ import sys
 def main():
     print("Sack Compiler!")
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         sys.exit("Error: Compiler needs source file as argument.")
+    if len(sys.argv) > 2:
+        sys.exit("Error: Compiler only takes 1 source file as a parameter.")
     with open(sys.argv[1], 'r') as inputFile:
         input = inputFile.read()
 
@@ -18,7 +20,7 @@ def main():
 
     parser.program() # Start the parser.
     emitter.writeFile() # Write the output to file.
-    print("Compiling completed.")
+    print("Compiling completed. Ouptuted file: out.c")
     #exec(open("out.py").read())
 
 if __name__ == '__main__':
